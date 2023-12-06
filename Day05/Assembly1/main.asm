db648334bb3f81ca634d75ae4f2cb8b0c3afa458
doxakis1 <mike.karat@yahoo.com>
2023-12-07 01:20:44 +0200

    32 bit is not strong enough, I NEED MORE POWER

--- Day05/Assembly1/main.asm
+++ Day05/Assembly1/main.asm
@@ -27,6 +27,7 @@ section .data
     filename db 'file.txt', 0
 section .bss
     fd    resd 1
+    s_index resd 1
     index resd 1
     nums1 resd BUFFER_SIZE
     nums2 resd BUFFER_SIZE
@@ -42,8 +43,9 @@ _start:
     sub esp, 4 ; index counter
     
     mov [esp], dword 0
-    mov [index], dword  0
     mov [fd], dword -1
+    mov [index], dword  0
+    mov [s_index], dword  0
 _open_file:
     mov eax, open
     mov ebx, filename
@@ -64,26 +66,26 @@ _get_all_seeds:
     call get_number
     cmp ecx, 1
     je _get_maps
-    mov edx, dword [index]
+    mov edx, dword [s_index]
     lea edi, [seeds + edx * 4]
     mov [edi], eax
     inc edx
-    mov [index], edx
+    mov [s_index], edx
     mov ecx, dword [esp]
     add ecx, ebx
     inc ecx
     mov [esp], ecx
     jmp _get_all_seeds
 _get_maps:
-    mov edx, [index]
+    mov edx, [s_index]
     lea edi, [seeds + edx * 4]
     mov [edi], dword -1
+    mov [s_index], dword 0
     mov eax, dword [fd]
     lea ebx, [buffer]
     call get_next_line
     cmp eax, 0
     je _close_file ; error case
-    mov [index], dword 0
 _get_next_map:
     mov eax, dword [fd]
     lea ebx, [buffer]
@@ -153,17 +155,90 @@ _got_maps:
     mov [edi], dword -2
     lea edi, [nums3 + edx * 4]
     mov [edi], dword -2
+    mov [index], dword 0
+    jmp _transform_seeds
+
+_transform_seeds:
+    mov edx, dword [s_index]
+    lea edi, [seeds+edx * 4]
+    mov eax, dword [edi]
+    cmp eax, dword -1
+    je _find_smallest
+    mov [index], dword  0
+    mov [esp], eax
+_transform_per_map:
+    mov edx, [index]
+    lea edi, [nums2 + edx * 4]
+    mov eax, dword [edi]
+    cmp eax, dword -2
+    jne _continue
+    mov ebx, dword [esp]
+    mov edx, dword [s_index]
+    lea edi, [seeds+edx*4]
+    mov [edi], ebx
+    inc edx
+    mov [s_index], edx
+    jmp _transform_seeds
+_continue:
+    cmp eax, dword -1
+    je _transform_loop_next_iter
+    mov ebx, dword [esp]
+    cmp ebx, eax
+    jl _transform_loop_next_iter
+    mov edx, dword [index]
+    lea edi, [nums3 + edx * 4]
+    mov eax, dword [edi]
+    cmp ebx, eax
+    jg _transform_loop_next_iter
+    jmp _found_place
+
+_transform_loop_next_iter:
+    mov edx, dword [index]
     inc edx
     mov [index], edx
-    jmp _close_file
-    
+    jmp _transform_per_map
+
+_found_place:
+    mov edx, dword [index]
+    lea edi, [nums2 + edx * 4]
+    mov ebx, dword [esp]
+    mov eax, dword [edi]
+    sub ebx, eax
+    lea edi, [nums1 + edx * 4]
+    mov eax, dword [edi]
+    add eax, ebx
+    mov [esp], eax
+_find_end:
+    lea edi, [nums1 + edx * 4]
+    mov eax, dword [edi]
+    cmp eax, 0
+    jl _found_end
+    inc edx
+    jmp _find_end
+_found_end:
+    mov [index], edx
+    jmp _transform_per_map
+
+_find_smallest:
+    mov [esp], dword 2147483647 ; int max
+    mov ecx, dword 0
+_find_smallest_loop:
+    lea edi, [seeds + ecx * 4]
+    mov eax, dword [edi]
+    cmp eax, -1
+    je _close_file
+    inc ecx
+    mov ebx, dword [esp]
+    cmp eax, ebx
+    jge _find_smallest_loop
+    mov [esp], eax
+    jmp _find_smallest_loop
 
 _close_file:
-    mov esi, seeds
-    mov esi, buffer
-    mov esi, nums1
-    mov esi, nums2
-    mov esi, nums3
+    mov eax, dword [esp]
+    mov ebx, dword 1
+    call fd_printnum
+    lea esi, [seeds]
     mov eax, close
     mov ebx, [fd]
     int 0x80
