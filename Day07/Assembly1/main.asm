8cabebc1f42c72d779fa67004ccbb58a292feb3b
doxakis1 <mike.karat@yahoo.com>
2023-12-23 13:56:13 +0200

    Allocator added, been busy irl will try to get these advent of code challenges done at some point o.O

--- Day07/Assembly1/main.asm
+++ Day07/Assembly1/main.asm
@@ -1,29 +1,13 @@
 extern fd_printnum
-
+extern reserve_init
 section .data
     filename db 'file.txt'
-section .bss
-    original_brk resd 1
 section .text
     global _start
 
 
 _start:
-    mov eax, 45
-    mov ebx, 0
-    int 0x80
-    
-    mov [original_brk], eax
-    add eax, 45
-    mov ebx, eax
-    mov eax, 45
-    int 0x80
-    mov ebx, 1
-    call fd_printnum
-    mov eax, dword [original_brk]
-    lea esi, [eax]
-    mov [esi], dword 1
-    
+    call reserve_init
 _exit:
     mov eax, 1
     mov ebx, 0
--- Day07/Assembly1/reserve.asm
+++ Day07/Assembly1/reserve.asm
@@ -1,9 +1,42 @@
+%define RESERVE_SIZE_INITIAL 524288 ; 2^19
+%define sys_brk 45
 section .bss
     brk_vals resd 2
     head_chunck resd 2
 section .text
-    global reserved_init
+    global reserve_init
     global reserve
     global free
 
-reserved_init: ; returns -1 on eax if it fails allocates initial hap
+reserve_init: ; returns 0 on success and -1 for errors
+    push ebp
+    mov ebp, esp
+
+_get_original_brk:
+    mov eax, sys_brk
+    mov ebx, 0
+    int 0x80
+    jc _reserve_init_error ; if error occurs carry flag is set
+    lea esi, [brk_vals]
+    mov [esi], eax ; original brk
+_reserve_init_block:
+    add eax, dword RESERVE_SIZE_INITIAL
+    mov ebx, eax
+    mov eax, sys_brk
+    int 0x80
+    jc _reserve_init_error
+    lea esi, [brk_vals + 4]
+    mov [esi], ebx
+    lea esi, [head_chunck + 4]
+    mov [esi], dword RESERVE_SIZE_INITIAL
+    sub esi, 4
+    mov eax, dword [brk_vals]
+    mov [esi], eax
+    mov eax, dword 0
+    jmp _reserve_init_ret
+_reserve_init_error:
+    mov eax, dword -1
+_reserve_init_ret:
+    mov esp, ebp
+    pop ebp
+    ret
